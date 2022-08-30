a9937acdca65b12ae78bf2043a1d99065dd7bb05
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-08-30 23:51:08 +0900

    add react-intersection-observer

--- src/components/templates/HomeLayout.tsx
+++ src/components/templates/HomeLayout.tsx
@@ -1,26 +1,15 @@
 import styled from '@emotion/styled'
-import { useEffect, useRef, useState } from 'react'
 import { use100vh } from 'react-div-100vh'
 
-import useChangeColor from '@/hooks/useChangeColor'
 import { AboutMe } from '@/components/organisms/top/AboutMe'
 import { HeroArea } from '@/components/organisms/top/HeroArea'
 import { Work } from '@/components/organisms/top/Work'
 
 export const HomeLayout = () => {
   const height = use100vh()
-  const elm = useRef<HTMLDivElement>(null)
-  const [elmHeight, setElmHeight] = useState<number>(0)
-
-  useEffect(() => {
-    if (!elm.current) return
-    setElmHeight(elm.current.getBoundingClientRect().height)
-  }, [])
-
-  const color = useChangeColor(elmHeight)
 
   return (
-    <_FixedBg height={height ? `${height}px` : '100vh'} color={color} ref={elm}>
+    <_FixedBg height={height ? `${height}px` : '100vh'} color={'#fff'}>
       <HeroArea />
       <Work />
       <AboutMe />
