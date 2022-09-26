18cbe1d052d8d6f8857c32198bd770ee6d1749ea
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-09-26 12:36:39 +0900

    update works area

--- src/components/organisms/profile/MyProfileBlock.tsx
+++ src/components/organisms/profile/MyProfileBlock.tsx
@@ -13,38 +13,38 @@ type Props = {
 export const MyProfileBlock = ({ children }: Props) => {
   return (
     <_Content>
-      <_Detail>
-        <_ImgWrap>
-          <_ImageFigure>
-            <Image src={profile} alt={'プロフィール写真'} layout="fill" />
-          </_ImageFigure>
-        </_ImgWrap>
-        <_TextWrap>
-          <_NameWrap>
-            <_MyName>磯野 圭希</_MyName>
-            <TextEg fontSize="14px">Isono Tamaki</TextEg>
-          </_NameWrap>
-          {children}
-        </_TextWrap>
-      </_Detail>
+      <_ImgWrap>
+        <_ImageFigure>
+          <Image src={profile} alt={'プロフィール写真'} layout="fill" />
+        </_ImageFigure>
+      </_ImgWrap>
+      <_TextWrap>
+        <_NameWrap>
+          <_MyName>磯野 圭希</_MyName>
+          <TextEg fontSize="14px">Isono Tamaki</TextEg>
+        </_NameWrap>
+        {children}
+      </_TextWrap>
     </_Content>
   )
 }
 
 const _Content = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-`
 
-const _Detail = styled.div`
-  display: flex;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
 `
 
 const _ImgWrap = styled.div`
   display: grid;
   width: 460px;
   place-content: center;
+
+  @media (max-width: 960px) {
+    width: 100%;
+  }
 `
 
 const _ImageFigure = styled.figure`
@@ -57,6 +57,10 @@ const _TextWrap = styled.div`
   display: flex;
   width: 460px;
   flex-direction: column;
+
+  @media (max-width: 960px) {
+    width: 100%;
+  }
 `
 
 const _NameWrap = styled.div`
--- src/components/organisms/top/Work.tsx
+++ src/components/organisms/top/Work.tsx
@@ -7,6 +7,7 @@ import StickyBox from 'react-sticky-box'
 import iromemoImg from '/public/work/16memo.jpg'
 import geikousaiImg from '/public/work/geikosaiPre.jpg'
 import arestImg from '/public/work/ARest.jpg'
+import { Heading } from '@/components/atoms/text/Heading'
 import { WorksPagination } from '@/components/molecules/WorksPagination'
 
 type Props = {
@@ -48,48 +49,87 @@ export const Work = ({ workRef, scrollY }: Props) => {
   return (
     <>
       {height && (
+        // <_Section ref={workRef}>
+        //   <_ScrollArea height={height ? `${height * 4}px` : '100vh'}>
+        //     <_WrapperIromemo
+        //       height={height ? `${height}px` : '100vh'}
+        //       isFix={height <= scrollY && scrollY < height * 2}
+        //       isView={isIromemoView}
+        //     >
+        //       <Heading fontSize="45px">Works</Heading>
+        //       <_ImgInner>
+        //         <img src={iromemoImg.src} alt="16memo" />
+        //       </_ImgInner>
+        //     </_WrapperIromemo>
+        //     <_WrapperGeiko
+        //       height={height ? `${height}px` : '100vh'}
+        //       isFix={height * 2 <= scrollY && scrollY < height * 3}
+        //       isView={isGeikosaiView}
+        //     >
+        //       <Heading fontSize="45px">Works</Heading>
+        //       <_ImgInner>
+        //         <img src={geikousaiImg.src} alt="geikousai pre" />
+        //       </_ImgInner>
+        //     </_WrapperGeiko>
+        //     <_WrapperARest
+        //       height={height ? `${height}px` : '100vh'}
+        //       isFix={height * 3 <= scrollY && scrollY < height * 4}
+        //       isView={isARestView}
+        //     >
+        //       <Heading fontSize="45px">Works</Heading>
+        //       <_ImgInner>
+        //         <img src={arestImg.src} alt="A Rest" />
+        //       </_ImgInner>
+        //     </_WrapperARest>
+        //     <_WorkSticky
+        //       height={height ? `${height}px` : '100vh'}
+        //       isView={height * 4 <= scrollY}
+        //     >
+        //       <_ImgInner>
+        //         <img src={arestImg.src} alt="A Rest" />
+        //       </_ImgInner>
+        //     </_WorkSticky>
+        //   </_ScrollArea>
+        // </_Section>
         <_Section ref={workRef}>
-          <StickyBox style={{ width: '20vw' }}>
-            <WorksPagination height={height ? `${height}px` : '100vh'} />
-          </StickyBox>
           <_ScrollArea height={height ? `${height * 4}px` : '100vh'}>
-            <Link href={'/'}>
-              <_WrapperIromemo
-                height={height ? `${height}px` : '100vh'}
-                isFix={height <= scrollY && scrollY < height * 2}
-                isView={isIromemoView}
-              >
-                <_ImgInner>
-                  <img src={iromemoImg.src} alt="16memo" />
-                </_ImgInner>
-              </_WrapperIromemo>
-            </Link>
-            <Link href={'/'}>
-              <_WrapperGeiko
-                height={height ? `${height}px` : '100vh'}
-                isFix={height * 2 <= scrollY && scrollY < height * 3}
-                isView={isGeikosaiView}
-              >
-                <_ImgInner>
-                  <img src={geikousaiImg.src} alt="geikousai pre" />
-                </_ImgInner>
-              </_WrapperGeiko>
-            </Link>
-            <Link href={'/'}>
-              <_WrapperARest
-                height={height ? `${height}px` : '100vh'}
-                isFix={height * 3 <= scrollY && scrollY < height * 4}
-                isView={isARestView}
-              >
-                <_ImgInner>
-                  <img src={arestImg.src} alt="A Rest" />
-                </_ImgInner>
-              </_WrapperARest>
-            </Link>
+            <_StickyHeading isFix={height <= scrollY && scrollY <= height * 4}>
+              <Heading fontSize="45px">Works</Heading>
+            </_StickyHeading>
+            <_WrapperIromemo
+              height={height ? `${height}px` : '100vh'}
+              isFix={height <= scrollY && scrollY < height * 2}
+              isView={isIromemoView}
+            >
+              <_ImgInner>
+                <img src={iromemoImg.src} alt="16memo" />
+              </_ImgInner>
+            </_WrapperIromemo>
+            <_WrapperGeiko
+              height={height ? `${height}px` : '100vh'}
+              isFix={height * 2 <= scrollY && scrollY < height * 3}
+              isView={isGeikosaiView}
+            >
+              <_ImgInner>
+                <img src={geikousaiImg.src} alt="geikousai pre" />
+              </_ImgInner>
+            </_WrapperGeiko>
+            <_WrapperARest
+              height={height ? `${height}px` : '100vh'}
+              isFix={height * 3 <= scrollY && scrollY < height * 4}
+              isView={isARestView}
+            >
+              <_ImgInner>
+                <img src={arestImg.src} alt="A Rest" />
+              </_ImgInner>
+            </_WrapperARest>
             <_WorkSticky
               height={height ? `${height}px` : '100vh'}
               isView={height * 4 <= scrollY}
             >
+              <_StickyHeading isFix={false}>
+                <Heading fontSize="45px">Works</Heading>
+              </_StickyHeading>
               <_ImgInner>
                 <img src={arestImg.src} alt="A Rest" />
               </_ImgInner>
@@ -102,15 +142,19 @@ export const Work = ({ workRef, scrollY }: Props) => {
 }
 
 const _Section = styled.section`
-  display: flex;
   width: 100vw;
-  align-items: flex-start;
 `
 
 const _ScrollArea = styled.div<{ height: string }>`
   position: relative;
-  width: 60vw;
   height: ${(props) => props.height};
+`
+
+const _StickyHeading = styled.div<{ isFix: boolean }>`
+  position: ${(props) => (props.isFix ? 'fixed' : 'absolute')};
+  top: 60px;
+  left: 50%;
+  transform: translate(-50%, 0);
 `
 
 const _WrapperIromemo = styled.div<{
@@ -120,8 +164,9 @@ const _WrapperIromemo = styled.div<{
 }>`
   ${(props) => props.isFix && `position: fixed; top: 0;`}
   display: flex;
-  width: 60vw;
+  width: 100%;
   height: ${(props) => props.height};
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   opacity: ${(props) => (props.isView ? 1 : 0)};
@@ -134,8 +179,9 @@ const _WrapperGeiko = styled.div<{
 }>`
   ${(props) => props.isFix && `position: fixed; top: 0;`}
   display: flex;
-  width: 60vw;
+  width: 100%;
   height: ${(props) => props.height};
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   opacity: ${(props) => (props.isView ? 1 : 0)};
@@ -148,27 +194,24 @@ const _WrapperARest = styled.div<{
 }>`
   ${(props) => props.isFix && `position: fixed; top: 0;`}
   display: flex;
-  width: 60vw;
+  width: 100%;
   height: ${(props) => props.height};
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   opacity: ${(props) => (props.isView ? 1 : 0)};
 `
 
 const _WorkSticky = styled.div<{ height: string; isView: boolean }>`
+  position: relative;
   display: flex;
-  width: 60vw;
   height: ${(props) => props.height};
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   opacity: ${(props) => (props.isView ? 1 : 0)};
 `
 
 const _ImgInner = styled.figure`
-  width: 80%;
-  img {
-    width: 100%;
-    aspect-ratio: 1.618 / 1;
-    object-fit: cover;
-  }
+  max-width: 800px;
 `
