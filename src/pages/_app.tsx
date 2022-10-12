dc6b3d5b7e6e4daff4a8fd49acd5cb2f0e4f95ae
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-10-13 00:04:19 +0900

    remove chakraUI framer-motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import { ThemeProvider, Global, css } from '@emotion/react'
 import emotionReset from 'emotion-reset'
 import type { AppProps } from 'next/app'
@@ -8,28 +7,26 @@ import { theme } from '@/themes/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <ThemeProvider theme={theme}>
-        <Global
-          styles={css`
-            ${emotionReset}
-            body {
-              background-color: #fafafc;
-            }
-            li {
-              list-style: none;
-            }
-            a {
-              text-decoration: none;
-            }
-          `}
-        />
-        <main>
-          <Header />
-          <Component {...pageProps} />
-        </main>
-      </ThemeProvider>
-    </ChakraProvider>
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          ${emotionReset}
+          body {
+            background-color: #fafafc;
+          }
+          li {
+            list-style: none;
+          }
+          a {
+            text-decoration: none;
+          }
+        `}
+      />
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
   )
 }
 
