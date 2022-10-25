f495d9db8afdc529a06b7f9e1370fd92dd666f05
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-10-26 01:08:03 +0900

    add SNSIcon to NavList

--- src/components/molecules/NavList.tsx
+++ src/components/molecules/NavList.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled'
 import Link from 'next/link'
 
+import GithubIcon from 'public/icons/github.svg'
+import TwitterIcon from 'public/icons/twitter.svg'
+
 export const NavList = () => {
   const navList = [
     { nav: 'Top', path: '' },
@@ -17,20 +20,32 @@ export const NavList = () => {
           </Link>
         </li>
       ))}
+      <_A href="https://github.com/pppeyo38">
+        <GithubIcon width={24} height={24} />
+      </_A>
+      <_A href="https://twitter.com/ume2_uguisu">
+        <TwitterIcon width={24} height={24} />
+      </_A>
     </_NavList>
   )
 }
 
 const _NavList = styled.ul`
   display: flex;
+  align-items: center;
   margin: 0 18px;
-  gap: 36px;
+  gap: 24px;
 `
 
 const _Item = styled.a`
+  margin: 0 4px;
   color: ${({ theme }) => theme.colors.black};
   cursor: pointer;
   font-family: ${({ theme }) => theme.fonts.Inter};
   font-size: 16px;
   font-weight: ${({ theme }) => theme.fontWt.Medium};
+`
+
+const _A = styled.a`
+  cursor: pointer;
 `
