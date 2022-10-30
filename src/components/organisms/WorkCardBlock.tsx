14cbe8d0f2a65416add1a35f455e7792b7e637d7
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-10-30 18:03:51 +0900

    update WorkCardBlock

--- src/components/organisms/WorkCardBlock.tsx
+++ src/components/organisms/WorkCardBlock.tsx
@@ -1,20 +1,24 @@
 import styled from '@emotion/styled'
+import useMedia from 'use-media'
 import { WorkCard } from '@/components/molecules/WorkCard'
+import { mediaQueries } from '@/themes/mediaQueries'
 import { WorksListProps } from '@/types/workTypes'
 
 export const WorkCardBlock = ({ worksList }: WorksListProps) => {
+  const isPc = useMedia(mediaQueries.pc)
+
   return (
     <_Wrapper>
       {worksList.map((item, index) => (
         <WorkCard key={index} {...item} />
       ))}
-      {worksList.length % 3 === 1 && (
+      {isPc && worksList.length % 3 === 1 && (
         <>
           <_Div />
           <_Div />
         </>
       )}
-      {worksList.length % 3 === 2 && <_Div />}
+      {isPc && worksList.length % 3 === 2 && <_Div />}
     </_Wrapper>
   )
 }
